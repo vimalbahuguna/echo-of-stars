2f032c45f34c3607e058b70e92e1d3cd909e4233
Vimlesh Bahuguna <bahuguna.vimal@gmail.com>
2025-09-19 07:44:10 +0530

    Cursor update-19092025744

---
+++ android/app/src/main/java/com/echoofstars/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.echoofstars.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,9 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.721d6be9c13c40b2b6c00d47ae060ee7',
-  appName: 'echo-of-stars',
-  webDir: 'dist',
-  bundledWebRuntime: false,
-  server: {
-    url: 'https://721d6be9-c13c-40b2-b6c0-0d47ae060ee7.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  }
+  appId: 'com.sosoracle.app',
+  appName: 'sos_oracle',
+  webDir: 'dist'
 };
 
 export default config;
